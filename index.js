ec2cfc21fc468dacbd7942dfee0e77cfd09a0390
Muhammad-BinMushtaq <muhammadbinmushtaq3@gmail.com>
2025-07-03 01:14:47 -0700

    Mongo db connected and all errors read

---
+++ index.js
@@ -0,0 +1,5 @@
+import dotenv from 'dotenv';
+import connect_DB from './src/db/index.js'
+
+dotenv.config()
+connect_DB()
---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from '../constants.js'
+
+
+ const connect_DB = async () => {
+    try {
+      const db= await mongoose.connect(`${process.env.MONGODB_URI}/DB_NAME`)
+      console.log(`connected at !! ${[db]}`)
+
+    } catch (error) {
+        console.log("there is some error", error)
+        process.exit(1)
+
+    }
+
+}
+
+export default connect_DB
